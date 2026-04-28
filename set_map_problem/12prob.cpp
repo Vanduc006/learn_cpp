9204d8153576e56fba929fefb571cc0c55a0a2ea
vanduc006 <bagiadaohoa@gmail.com>
2026-04-29 02:23:45 +0700

    set map problem solution 11->14

---
+++ set_map_problem/12prob.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("../input.txt","r",stdin);
+    freopen("../output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; int m; cin >> n >> m;
+    set<int> a; set<int> b;
+    set<int> result;
+
+    for (int i=0; i < n; i++) {
+        int value; cin >> value;
+        a.insert(value);
+    }
+
+    for (int i=0; i < m; i++) {
+        int value; cin >> value;
+        b.insert(value);
+    }
+
+    for (int x : a) {
+        if (a.count(x) && b.count(x)) {
+            continue;
+        } else result.insert(x);
+    }
+
+    for (int x : b) {
+        if (a.count(x) && b.count(x)) {
+            continue;
+        } else result.insert(x);
+    }
+
+    for (int x : result) cout << x << " ";
+} 
